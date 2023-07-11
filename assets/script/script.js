d3caa6ca64cc91c48da671c9d04eb6064916575d
Allan DEHORS <allandhs@MacBook-Pro-de-Allan.local>
2023-07-11 12:59:04 +0200

    ModalOk

--- assets/script/script.js
+++ assets/script/script.js
@@ -87,109 +87,110 @@ fetch('https://api.themoviedb.org/3/movie/upcoming?language=fr-FR&page=1', optio
 
             // Get the modal
 
+            // modal
+            // Get the modal
             let container = document.querySelector('#container');
-            console.log(movie.title)
-            container.innerHTML += `<div id="myModal${count}" class="modal">
-
-            <div class="modal-content">
-              <span class="close">&times;</span>
-              
-              <img src="" alt="" class="modalImg">
-                <p class="modalDescription">${movie.title}</p>
-                <p class="modalDate"></p>
-                <p class="modalNote"></p>
-            </div>`;
-
-
-
-            // Get the button that opens the modal
-            var btn = document.querySelectorAll(`.img`);
-            console.log(btn);
-
-            let id = this.id
-            console.log(id);
-
-            btn.forEach(element => {
-                element.addEventListener('click', function () {
-
-                    let Modal = document.getElementById(`myModal${count}`);
-                    Modal.style.display = "block";
-                })
-            })
+            console.log(movie.title);
+            container.innerHTML += `
+                <div id="myModal${count}" class="modal">
+                    <div class="modal-content">
+                    <span class="close">&times;</span>
+                    <img src="" alt="" class="modalImg">
+                    <p class="modalDescription">${movie.title}</p>
+                    <p class="modalDate"></p>
+                    <p class="modalNote"></p>
+                    </div>
+                </div>`;
 
 
 
-            // Get the <span> element that closes the modal
-            var span = document.getElementsByClassName('close')[0];
+        })
 
 
-            // When the user clicks on <span> (x), close the modal
-
-            span.addEventListener('click', function () {
-                Modal.style.display = "none";
-            })
-        });
-    })
-    .catch(err => console.error(err));
+        // Get the button that opens the modal
+        var btn = document.querySelectorAll('.img');
+        console.log(btn);
 
 
 
-// Fonction recherche de films
+        var modal;
 
-let button = document.querySelector('.button');
-
-button.addEventListener('click', function () {
-
-
-    let input = document.querySelector('.input').value;
-
-    fetch(`https://api.themoviedb.org/3/search/movie?query=${input}&include_adult=false&language=fr-FR&page=1`, options)
-        .then(response => response.json())
-        .then(data => {
-            console.log(data)
-            searchMovie.innerHTML = '';
-
-            data.results.forEach(movie => {
-                const movieElement = document.createElement('div');
-                movieElement.classList.add('movie');
-
-                const posterImage = document.createElement('img');
-                posterImage.setAttribute('id', 'myBtn3');
-                posterImage.src = `https://image.tmdb.org/t/p/original/${movie.backdrop_path}`;
-                movieElement.appendChild(posterImage);
-
-                const titleElement = document.createElement('p');
-                titleElement.textContent = movie.title;
-                movieElement.appendChild(titleElement);
-
-                const dateElement = document.createElement('p');
-                dateElement.textContent = movie.release_date;
-                movieElement.appendChild(dateElement);
-
-                const popularityElement = document.createElement('p');
-                popularityElement.textContent = `${movie.vote_average}`;
-                movieElement.appendChild(popularityElement);
-
-                searchMovie.appendChild(movieElement);
-
-                
-
-                // Condition pour afficher une image si le film n'en a pas
-                if (movie.backdrop_path === null) {
-                    posterImage.src = `/assets/img/image-not-found.jpg`;
-                }
-
-
+        btn.forEach((element, index) => {
+            element.addEventListener('click', function () {
+                const modal = document.getElementById(`myModal${index + 1}`);
+                modal.style.display = "block";
+                console.log(this)
             });
-
         });
-    News.style.display = 'none';
-    lastMovie.style.display = 'none';
-    popularMovie.style.display = 'none';
-    h2Popular.style.display = 'none';
-})
 
-    .catch(err => console.error(err));
+        // Get the <span> element that closes the modal
+        var span = document.getElementsByClassName('close')[0];
+
+        // When the user clicks on <span> (x), close the modal
+        container.addEventListener('click', function (event) {
+            if (event.target.classList.contains('close')) {
+                const modal = event.target.closest('.modal');
+                modal.style.display = "none";
+            }
+        });
+
+
+        // Fonction recherche de films
+
+        let button = document.querySelector('.button');
+
+        button.addEventListener('click', function () {
+
+
+            let input = document.querySelector('.input').value;
+
+            fetch(`https://api.themoviedb.org/3/search/movie?query=${input}&include_adult=false&language=fr-FR&page=1`, options)
+                .then(response => response.json())
+                .then(data => {
+                    console.log(data)
+                    searchMovie.innerHTML = '';
+
+                    data.results.forEach(movie => {
+                        const movieElement = document.createElement('div');
+                        movieElement.classList.add('movie');
+
+                        const posterImage = document.createElement('img');
+                        posterImage.setAttribute('id', 'myBtn3');
+                        posterImage.src = `https://image.tmdb.org/t/p/original/${movie.backdrop_path}`;
+                        movieElement.appendChild(posterImage);
+
+                        const titleElement = document.createElement('p');
+                        titleElement.textContent = movie.title;
+                        movieElement.appendChild(titleElement);
+
+                        const dateElement = document.createElement('p');
+                        dateElement.textContent = movie.release_date;
+                        movieElement.appendChild(dateElement);
+
+                        const popularityElement = document.createElement('p');
+                        popularityElement.textContent = `${movie.vote_average}`;
+                        movieElement.appendChild(popularityElement);
+
+                        searchMovie.appendChild(movieElement);
 
 
 
+                        // Condition pour afficher une image si le film n'en a pas
+                        if (movie.backdrop_path === null) {
+                            posterImage.src = `/assets/img/image-not-found.jpg`;
+                        }
+
+
+                    });
+
+                });
+            News.style.display = 'none';
+            lastMovie.style.display = 'none';
+            popularMovie.style.display = 'none';
+            h2Popular.style.display = 'none';
+        })
+
+            .catch(err => console.error(err));
+
+
+    })
